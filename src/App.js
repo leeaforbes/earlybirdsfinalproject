d2b555ece55f992fe806db2b43396b56bcec543a
BuildTools <unconfigured@null.spigotmc.org>
2023-03-25 12:11:44 -0500

    using sass to combine classes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+// https://react-bootstrap.github.io/getting-started/introduction/
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/scss/bootstrap.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="border bootstrap-center">
+      Early Birds Final Project
     </div>
   );
 }

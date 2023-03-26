307cd620ca21d06823768f5406fff78233ddfa43
BuildTools <unconfigured@null.spigotmc.org>
2023-03-26 01:06:24 -0500

    search works

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -3,9 +3,14 @@ import "./Sidebar.scss"
 
 const Sidebar = () => {
 	const searchFieldRef = useRef(null)
+	const [searchTerm, setSearchTerm] = useState("")
 	const [setList] = useState(["Dan's Bday", "Claudia's Baby Shower", "EMO NITE", "Festival of Lights"])
 	const [mixList] = useState(["Rhiannon x FEELWITCHU", "Fever - Peggy Lee House Remix", "Krusty Crab Pizza x Romanticist", "god i love edm", "red hot bars", "soul-ripping riffs"])
 	const [audioList] = useState(["rhiannon.mp4", "feelwu.mp4", "drumzzz.mp4", "krabpizza.mp4", "untitled4.mp4", "rhiannon2.mp4"])
+
+	function searchOnChange(){
+		setSearchTerm(searchFieldRef.current.value)
+	}
 
 	return (
 		<div className='sidebar-modal'>
@@ -17,38 +22,35 @@ const Sidebar = () => {
 				ref={searchFieldRef}
 				type='text'
 				placeholder='Search'
+				onChange={searchOnChange}
 			>
 			</input>
 
-			
-
-			<CreateList listName={"Sets"} list={setList} onClickFunction={() => {console.log("s")}} />
+			<CreateList listName={"Sets"} list={setList} searchTerm={searchTerm} />
 			<br/>
-			<CreateList listName={"Mixes"} list={mixList} onClickFunction={() => {console.log("m")}} />
+			<CreateList listName={"Mixes"} list={mixList} searchTerm={searchTerm} />
 			<br/>
-			<CreateList listName={"Audios"} list={audioList} onClickFunction={() => {console.log("a")}} />
+			<CreateList listName={"Audios"} list={audioList} searchTerm={searchTerm} />
 		</div>
 	)
 };
 
-function CreateList({listName, list, onClickFunction}) {
+function CreateList({listName, list, searchTerm}) {
 
 	const [showList, setShowList] = useState(true)
 
-	function collapseList() {
-
-	}
-
 	var outputList = []
   list.forEach(elementName => {
-    outputList.push(
-      <div
-        className=""
-        key={elementName}
-      >
-				{"┗ " + elementName}
-      </div>
-    )
+		if(elementName.toLowerCase().startsWith(searchTerm)){
+			outputList.push(
+				<div
+					className=""
+					key={elementName}
+				>
+					{"┗ " + elementName}
+				</div>
+			)
+		}
   });
 
   return (

199e98a9795314cc7c3d1ff49f72a25f4b3c60fa
BuildTools <unconfigured@null.spigotmc.org>
2023-03-25 20:54:34 -0500

    getting started

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss';
+import Sidebar from './Sidebar';
 
 // https://react-bootstrap.github.io/getting-started/introduction/
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,10 +7,24 @@ import 'bootstrap/scss/bootstrap.scss';
 
 function App() {
   return (
-    <div className="border bootstrap-center">
-      Early Birds Final Project
+    <div>
+      <div className="border bootstrap-center">
+        Early Birds Final Project
+      </div>
+
+      <button
+        onClick={() => loadSidebar()}
+      >
+        sidebar
+      </button>
+
+      <Sidebar></Sidebar>
     </div>
   );
+}
+
+function loadSidebar() {
+  console.log("load sidebar")
 }
 
 export default App;
